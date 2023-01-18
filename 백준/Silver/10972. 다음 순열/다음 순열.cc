f805e4f81e4cd4c1e75724dd1127c3cc13c58d5a
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-01-18 19:16:41 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int target[10001];
+
+
+void INPUT()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> target[i];
+}
+
+
+void SOLVE()
+{
+    if(next_permutation(target, target + n))
+        for(int i = 0; i < n; i++)
+            cout << target[i] << " ";
+    else cout << -1;
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
