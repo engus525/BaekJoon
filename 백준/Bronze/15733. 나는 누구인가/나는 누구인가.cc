3a9f6f17c9744e648e2d16e0e8b79ae00b8d9445
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-01-18 19:12:21 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+
+void INPUT()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+}
+
+
+void SOLVE()
+{
+    cout << "I'm Sexy";
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
