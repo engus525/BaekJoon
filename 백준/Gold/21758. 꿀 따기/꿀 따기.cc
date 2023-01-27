2eb650c40e6f3700597e90e076365783aaaa4a50
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-01-27 23:56:48 +0900

    [Gold V] Title: 꿀 따기, Time: 8 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Gold/21758. 꿀 따기/꿀 따기.cc
+++ 백준/Gold/21758. 꿀 따기/꿀 따기.cc
@@ -26,31 +26,22 @@ void SOLVE()
         goRight[i + 1] += goRight[i];
     }
 
-
     int ans = -1;
-
-    int sum;
-    //벌 한 마리가 왼쪽 끝에서 출발 -> 꿀통은 가장 오른쪽
     for(int i = 2; i <= n-1; i++)
     {
-        sum = 2 * goRight[n] - goRight[i] - (honey[i] + honey[1]);
+        //벌 한 마리가 왼쪽 끝에서 출발 -> 꿀통은 가장 오른쪽
+        int sum = 2 * goRight[n] - goRight[i] - (honey[i] + honey[1]);
         ans = max(ans, sum);
-    }
 
-    //벌 한 마리가 오른쪽 끝에서 출발 -> 꿀통은 가장 왼쪽
-    for(int i = 2; i <= n-1; i++)
-    {
+        //벌 한 마리가 오른쪽 끝에서 출발 -> 꿀통은 가장 왼쪽
         sum = 2 * goLeft[1] - goLeft[i] - (honey[i] + honey[n]);
         ans = max(ans, sum);
-    }
 
-    //벌이 양쪽 끝에서 출발 -> 꿀통 위치에 따라 다름
-    for(int i = 2; i <= n-1; i++)
-    {
+        //벌이 양쪽 끝에서 출발 -> 꿀통 위치에 따라 다름
         sum = (goRight[i] - honey[1]) + (goLeft[i] - honey[n]);
         ans = max(ans,sum);
     }
-
+    
     cout << ans;
 }
 
