d1d5c37e16c0c7dfe16dd9a7dacf674d17e7a2dc
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-02-02 00:51:41 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 16 ms, Memory: 2332 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int n;
+int h[80001];
+
+typedef long long ll;
+stack<pair<int,ll>> s;//{건물의 높이,볼 수 있는 옥상의 수}
+
+void INPUT()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> h[i];
+}
+
+
+void SOLVE()
+{
+    ll ans = 0;
+    for(int i = n-1; i >= 0; i--)
+    {
+        ll cnt = 0;
+        while(!s.empty() && h[i] > s.top().first)
+        {
+            //{(현재 높이)에서 (볼 수 있는 옥상)에서 (볼 수 있는 옥상의 수)} + 1
+            cnt += s.top().second + 1;
+            //오른쪽 건물이 현재 건물에 count됨으로써 고려하지 않아도됨
+            s.pop();
+        }
+        s.push({h[i],cnt});
+        ans += cnt;
+    }
+    cout << ans;
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
