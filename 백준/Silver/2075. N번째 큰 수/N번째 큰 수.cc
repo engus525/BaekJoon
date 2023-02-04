476171accac3f6b7762c4d58bef4bb5797cfe968
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-02-05 01:11:09 +0900

    [Silver II] Title: N번째 큰 수, Time: 356 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -4,6 +4,7 @@ using namespace std;
 
 typedef long long ll;
 int n;
+//오름차순 정렬
 priority_queue<ll,vector<ll>,greater<ll>> pq;
 
 void INPUT()
@@ -14,6 +15,10 @@ void INPUT()
     {
         int num; cin >> num;
         pq.push(num);
+        /*
+        n번째로 큰 수는 어떤 수가 삽입되든 pq.top()으로부터 n개의 원소 내에 위치한다.
+        따라서 n개 이상의 수가 삽입되면 즉시 pop 해준다.
+        */
         if(pq.size() > n) pq.pop();
     }
 }
@@ -21,6 +26,7 @@ void INPUT()
 
 void SOLVE()
 {
+    //pq에는 n개의 수가 남아있게 되고, 오름차순 정렬이므로 top에 n번째 큰 수가 위치한다.
     cout << pq.top();
 }
 
