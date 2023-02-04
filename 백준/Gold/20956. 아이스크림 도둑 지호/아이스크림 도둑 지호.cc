a2bfb7eb75a1fe8299c3da4a8e0d1a4d5a463996
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-02-04 22:41:05 +0900

    [Gold IV] Title: 아이스크림 도둑 지호, Time: 72 ms, Memory: 4828 KB -BaekjoonHub

---
+++ 백준/Gold/20956. 아이스크림 도둑 지호/아이스크림 도둑 지호.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+int A;
+
+typedef pair<int,int> pii;
+struct comp
+{
+    bool operator()(pii a,pii b)
+    {
+        if(a.first == b.first) return a.second > b.second;//오름차순
+        else return a.first < b.first;//내림차순
+    }
+};
+priority_queue<pii,vector<pii>,comp> minHeap;//작은 인덱스가 먼저
+priority_queue<pii> maxHeap;//큰 인덱스가 먼저
+bool visited[100001];
+bool isReversed = false;
+
+void INPUT()
+{
+    //ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    scanf("%d %d",&n,&m);
+    for(int i = 1; i <= n; i++)
+    {
+        scanf("%d", &A);
+        minHeap.push({A,i});
+        maxHeap.push({A,i});
+    }
+}
+
+
+void SOLVE()
+{
+    while(m--)
+    {
+        if(isReversed)
+        {
+            while(visited[maxHeap.top().second]) maxHeap.pop();
+            if(maxHeap.top().first % 7 == 0) isReversed = !isReversed;
+            printf("%d\n",maxHeap.top().second);
+            visited[maxHeap.top().second] = true;
+            maxHeap.pop();
+        }
+        else
+        {
+            while(visited[minHeap.top().second]) minHeap.pop();
+            if(minHeap.top().first % 7 == 0) isReversed = !isReversed;
+            printf("%d\n",minHeap.top().second);
+            visited[minHeap.top().second] = true;
+            minHeap.pop();
+        }
+    }
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
