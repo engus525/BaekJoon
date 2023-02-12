ac814664ea0f33ea957f11c07cbdfea4547a6ba0
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-02-13 04:32:19 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+
+
+void INPUT()
+{
+	IAMFAST
+}
+
+void SOLVE()
+{
+	cout<<2023<<'\n'<<02<<'\n'<<12;
+}
+
+int main()
+{
+	INPUT();
+	SOLVE();
+}
