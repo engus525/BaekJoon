a31d449026f7d0ba7d48308cb1f6007631b38e72
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-02-26 21:49:57 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -1,14 +1,5 @@
 #include<iostream>
-#include<string>
-#include<algorithm>
-#include<cmath>
-#include<vector>
-#include<stdio.h>
-#include<ctime>
-#include<numeric>
-#include<stack>
 using namespace std;
-/* –2,147,483,648 ~ 2,147,483,647 */
 
 int h, w;
 int block[501][501];
@@ -30,9 +21,10 @@ void INPUT()
 bool isBlocked(int r, int c)
 {
 	bool left = false, right = false;
-	if (c == 1 || c == w)
-		return false;
+	// 양 끝자리면 빗물이 고일 수 없음
+	if (c == 1 || c == w) return false;
 
+	// 왼쪽
 	for (int i = c - 1; i >= 1; i--)
 		if (block[r][i] == 1)
 		{
@@ -40,6 +32,7 @@ bool isBlocked(int r, int c)
 			break;
 		}
 
+	// 오른쪽
 	for (int i = c + 1; i <= w; i++)
 		if (block[r][i] == 1)
 		{
@@ -52,27 +45,17 @@ bool isBlocked(int r, int c)
 
 void solve()
 {
+	// 좌측 하단부터 우측으로 올라가며 탐색
 	for(int i = h; i > 0; i--)
 		for (int j = 1; j <= w; j++)
 			if (block[i][j] == 0 && isBlocked(i, j))
 				ans++;
 
 	cout << ans;
-
-	/*for (int i = 1; i <= h; i++)
-	{
-		cout << '\n';
-		for (int j = 1; j <= w; j++)
-		{
-			cout << block[i][j] << " ";
-		}
-	}*/
-
 }
 
 int main()
 {
 	INPUT();
-
 	solve();
 }
