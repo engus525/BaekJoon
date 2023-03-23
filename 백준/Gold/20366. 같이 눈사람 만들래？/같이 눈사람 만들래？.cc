c7d75c1c89e3ce592f58766372d8c271dabcfa3a
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-03-23 23:02:21 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 24 ms, Memory: 6764 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int n;
+typedef long long ll;
+int snow[601];
+int ans = 2e9;
+struct SNOW
+{
+    int x,y,sum;
+};
+bool comp(SNOW a,SNOW b)
+{
+    return a.sum < b.sum;
+}
+vector<SNOW> v;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> snow[i];
+}
+
+
+void SOLVE()
+{
+    for(int i = 0; i < n-1; i++)
+        for(int j = i+1; j < n; j++)
+            v.push_back({i, j, snow[i] + snow[j]});
+    sort(v.begin(),v.end(),comp);
+
+
+    for(int snow1 = 0; snow1 < v.size()-1; snow1++)
+        for(int snow2 = snow1+1; snow2 < v.size(); snow2++)
+        {
+            if(v[snow1].x == v[snow2].x) continue;
+            if(v[snow1].y == v[snow2].y) continue;
+            if(v[snow1].y == v[snow2].x) continue;
+            if(v[snow1].y == v[snow2].x) continue;
+
+            ans = min(ans,v[snow2].sum-v[snow1].sum);
+            break;
+        }
+
+    cout << ans;
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
