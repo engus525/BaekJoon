e65d481cebab7dae49ef62502286a5b1023f42c1
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-03-24 16:10:37 +0900

    [Silver IV] Title: 개미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <math.h>
+using namespace std;
+
+int w, h, p, q, t;
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	
+	cin >> w >> h >> p >> q >> t;
+	
+	p += t; q += t;
+	p %= 2 * w; q %= 2 * h;
+	if (p > w)
+		p = 2 * w - p;
+	if (q > h)
+		q = 2 * h - q;
+
+	cout << p << " " << q;
+}
