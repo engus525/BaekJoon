e4a60ababddbe7b8b6d5f08ab682422945c64103
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-05-11 02:16:05 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int a,b;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> a >> b;
+}
+
+
+void SOLVE()
+{
+    cout << a*b;
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
