88f9c9a010d16a20e6beeab406516c411356334a
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-05-12 14:28:20 +0900

    [Gold IV] Title: 돌 그룹, Time: 56 ms, Memory: 10368 KB -BaekjoonHub

---
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int a,b,c;
+bool visited[1501][1501];
+struct Rock
+{
+    int a,b,c;
+};
+
+void INPUT()
+{
+    IAMFAST
+    cin >> a >> b >> c;
+}
+
+bool BFS()
+{
+    queue<Rock> q;
+    q.push({a,b,c});
+
+    while(!q.empty())
+    {
+        auto [x,y,z] = q.front();
+        q.pop();
+
+        if(x==y && y==z) return true;
+
+        if(visited[x][y]) continue;
+        visited[x][y] = true;
+
+        if((x != 2*y) && y && (x > y)) q.push({x-y,y+y,z});
+        if((y != 2*x) && x && (x < y)) q.push({x+x,y-x,z});
+        if((y != 2*z) && z && (y > z)) q.push({x,y-z,z+z});
+        if((z != 2*y) && y && (y < z)) q.push({x,y+y,z-y});
+        if((z != 2*x) && x && (z > x)) q.push({x+x,y,z-x});
+        if((x != 2*z) && z && (z < x)) q.push({x-z,y,z+z});
+    }
+    return false;
+}
+
+void SOLVE()
+{
+    cout << BFS();
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
