bbc676a51749b6a0d947bcbe6f1227b7da55372b
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-05-12 14:29:31 +0900

    [Gold IV] Title: 돌 그룹, Time: 52 ms, Memory: 10368 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.cc
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -32,12 +32,12 @@ bool BFS()
         if(visited[x][y]) continue;
         visited[x][y] = true;
 
-        if((x != 2*y) && (x > y)) q.push({x-y,y+y,z});
-        if((y != 2*x) && (x < y)) q.push({x+x,y-x,z});
-        if((y != 2*z) && (y > z)) q.push({x,y-z,z+z});
-        if((z != 2*y) && (y < z)) q.push({x,y+y,z-y});
-        if((z != 2*x) && (z > x)) q.push({x+x,y,z-x});
-        if((x != 2*z) && (z < x)) q.push({x-z,y,z+z});
+        if(x > y) q.push({x-y,y+y,z});
+        if(x < y) q.push({x+x,y-x,z});
+        if(y > z) q.push({x,y-z,z+z});
+        if(y < z) q.push({x,y+y,z-y});
+        if(z > x) q.push({x+x,y,z-x});
+        if(z < x) q.push({x-z,y,z+z});
     }
     return false;
 }
