e3a2f7be7b32c867115b83ce0f2b1a6adfa2ad64
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-05-27 02:29:18 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int n;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+void SOLVE()
+{
+    while(n--) cout << "SciComLove\n";
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
