f2051214f0474933a138dab5fae41c2ed7a564a3
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-06-13 03:00:07 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int n;
+vector<int> v;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+
+void SOLVE()
+{
+    for(int i = 0; i < n; i++)
+    {
+        int a; cin >> a;
+        if(v.empty() || v[v.size()-1] < a) v.emplace_back(a);
+        else
+        {
+            int idx = lower_bound(v.begin(), v.end(), a) - v.begin();
+            v[idx] = a;
+        }
+    }
+
+    cout << v.size();
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
