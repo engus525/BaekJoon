118a306d9a2c9d420548ea5fa37f2d5f0790a8cb
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-06-13 00:46:03 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 168 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int n;
+vector<int> v;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+
+void SOLVE()
+{
+    v.emplace_back(0);
+    for(int i = 0; i < n; i++)
+    {
+        int a; cin >> a;
+        if(v[v.size()-1] < a) v.emplace_back(a);
+        else
+        {
+            int idx = lower_bound(v.begin(), v.end(), a) - v.begin();
+            v[idx] = a;
+        }
+    }
+    cout << v.size()-1;
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
