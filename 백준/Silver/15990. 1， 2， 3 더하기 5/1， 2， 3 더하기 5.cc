639e8129e53612b5f30603fe44eb30d0cf1c5dc9
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-06-14 03:18:47 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 16 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int t;
+int dp[1000001][4];
+#define MOD 1'000'000'009
+
+void INPUT()
+{
+    IAMFAST
+    cin >> t;
+}
+
+
+void SOLVE()
+{
+    dp[1][1] = 1, dp[1][2] = 0, dp[1][3] = 0;
+    dp[2][1] = 0, dp[2][2] = 1, dp[2][3] = 0;
+    dp[3][1] = 1, dp[3][2] = 1, dp[3][3] = 1;
+
+    for(int i = 4; i <= 1000000; i++)
+    {
+        dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % MOD;
+        dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % MOD;
+        dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % MOD;
+    }
+
+
+    while(t--)
+    {
+        int n; cin >> n;
+        int ans = (((dp[n][1] + dp[n][2]) % MOD) + dp[n][3]) % MOD;
+        cout << ans << '\n';
+    }
+}
+
+int main()
+{
+    INPUT();
+    SOLVE();
+}
