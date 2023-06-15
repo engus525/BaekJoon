4ee997ee6eb4415f49d4bb0fefd5e4255c4af024
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-06-15 15:11:53 +0900

    [Gold II] Title: 보석 줍기, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2208. 보석 줍기/보석 줍기.cc
+++ 백준/Gold/2208. 보석 줍기/보석 줍기.cc
@@ -5,7 +5,6 @@ using namespace std;
 #define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
 
 int n,m;
-int jew[100001];
 int ps[100001];
 int dp[100001];
 
@@ -14,8 +13,8 @@ void INPUT()
     IAMFAST
     cin >> n >> m;
     for(int i = 1; i <= n; i++)
-        cin >> jew[i],
-        ps[i] = jew[i] + ps[i-1];
+        cin >> ps[i],
+        ps[i] += ps[i-1];
 }
 
 
@@ -26,7 +25,7 @@ void SOLVE()
     int ans = 0;
     for(int i = m; i <= n; i++)
     {
-        dp[i] = max(dp[i-1] + jew[i], ps[i] - ps[i - m]);
+        dp[i] = max(dp[i-1] + (ps[i] - ps[i-1]), ps[i] - ps[i - m]);
         ans = max(ans, dp[i]);
     }
     cout << ans;
