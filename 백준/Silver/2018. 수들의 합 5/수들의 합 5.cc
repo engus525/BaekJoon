47b9ce38368470f4aca0c30d08a94139f11baf56
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-07-10 04:30:30 +0900

    [Silver V] Title: 수들의 합 5, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.cc
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <string>
-#include <memory.h>
-#include <algorithm>
 
 using namespace std;
 #define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
@@ -19,14 +16,13 @@ void solution()
     int ans = 1;
 
     int left = 1, right = 2;
-    int result = left+right;
+    int result = left + right;
     while (left < right && right <= n)
     {
-
         if (result < n) result += ++right;
         else
         {
-            if(result == n) ans++;
+            if (result == n) ans++;
             result -= left++;
         }
     }
