66c38ec71883e445d2e4f0d900fa6fe09d3a3150
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-07-19 18:40:45 +0900

    [Gold IV] Title: 데이터 체커, Time: 64 ms, Memory: 11368 KB -BaekjoonHub

---
+++ 백준/Gold/22942. 데이터 체커/데이터 체커.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <memory.h>
+#include <cmath>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int n;
+struct Circle
+{
+    int pos;
+    bool isOpen;
+    int order;
+};
+vector<Circle> v;
+stack<pii> st;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int x,r; cin >> x >> r;
+        v.push_back({x - r, true, i});
+        v.push_back({x + r, false, i});
+    }
+}
+
+void solution()
+{
+    sort(v.begin(),v.end(),
+         [](Circle &a, Circle &b){return a.pos < b.pos;});
+
+    bool ans = true;
+    for(int i = 1; i < v.size(); i++)
+        if(v[i-1].pos == v[i].pos) ans = false;
+    for(int i = 0; i < v.size(); i++)
+    {
+//        cout << v[i].pos << " " << v[i].isOpen << " " << v[i].order << '\n';
+        if(v[i].isOpen)
+        {
+            st.emplace(v[i].pos,v[i].order);
+        }
+        else
+        {
+            if(st.empty()) ans = false;
+            else if(st.top().second == v[i].order) st.pop();
+            else ans = false;
+        }
+    }
+    if(ans) cout << "YES";
+    else cout << "NO";
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
