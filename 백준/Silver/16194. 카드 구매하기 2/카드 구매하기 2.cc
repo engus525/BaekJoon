49bb0a1a741605ab9255ba731676803abac75292
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-07-22 14:41:37 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 4 ms, Memory: 5972 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
@@ -1,18 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <map>
-#include <set>
-#include <string>
 #include <algorithm>
-#include <memory.h>
-#include <cmath>
 
 using namespace std;
 #define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
-typedef long long ll;
-typedef pair<int, int> pii;
 
 int n;
 int cost[10001];
@@ -30,6 +20,7 @@ void solution()
 {
     for (int i = 1; i <= n; i++) dp[1][i] = cost[1] * i;
 
+    int ans = 2e9;
     for (int i = 2; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
@@ -40,11 +31,10 @@ void solution()
                 dp[i][j] = min(min(dp[i][j],
                                    dp[i][j - i] + cost[i]),
                                dp[i - 1][j - i] + cost[i]);
+            
+            if(j == n) ans = min(ans, dp[i][n]);
         }
     }
-
-    int ans = 2e9;
-    for(int i = 1; i <= n; i++) ans = min(ans, dp[i][n]);
     cout << ans;
 }
 
