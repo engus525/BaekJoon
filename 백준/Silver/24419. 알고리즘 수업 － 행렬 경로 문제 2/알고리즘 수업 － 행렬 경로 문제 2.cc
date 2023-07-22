4309a5a408fbec9c45bd4f819df50199bdc54d13
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-07-22 14:30:16 +0900

    [Silver III] Title: 알고리즘 수업 - 행렬 경로 문제 2, Time: 92 ms, Memory: 9864 KB -BaekjoonHub

--- 백준/Silver/24419. 알고리즘 수업 － 행렬 경로 문제 2/알고리즘 수업 － 행렬 경로 문제 2.cc
+++ 백준/Silver/24419. 알고리즘 수업 － 행렬 경로 문제 2/알고리즘 수업 － 행렬 경로 문제 2.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 #define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
@@ -20,12 +20,10 @@ void INPUT()
 
 void solution()
 {
+    dp[1][0] = 1;
     for (int i = 1; i <= n+1; i++)
         for (int j = 1; j <= n+1; j++)
-        {
-            if(i == 1 || j == 1) dp[i][j] = 1;
-            else dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
-        }
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
     cout << dp[n+1][n+1] << " " << n*n;
 
 }
