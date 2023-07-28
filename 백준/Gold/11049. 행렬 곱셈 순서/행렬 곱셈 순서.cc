62f6e7ab55f015d4fbdbc93d8d819800f53ca1e8
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-07-29 02:57:43 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 56 ms, Memory: 3988 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <memory.h>
+#include <cmath>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+ll n;
+pll mat[501];
+ll dp[501][501];
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> mat[i].first >> mat[i].second;
+}
+
+
+void solution()
+{
+    for (int gap = 1; gap <= n; gap++)
+    {
+        for(int left = 1; left + gap <= n; left++)
+        {
+            int right = left + gap;
+            dp[left][right] = 2e10;
+            for(int cri = left; cri <= right; cri++)
+            {
+                dp[left][right] = min(dp[left][right],
+                                      dp[left][cri] +
+                                      dp[cri+1][right] +
+                                      mat[left].first * mat[cri].second * mat[right].second);
+            }
+        }
+    }
+    cout << dp[1][n];
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
