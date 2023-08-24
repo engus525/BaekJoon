109e5722c62c6a57e09289bf6c20f1835648c1b0
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-08-25 03:56:25 +0900

    [Gold V] Title: Heat Wave, Time: 0 ms, Memory: 2348 KB -BaekjoonHub

---
+++ 백준/Gold/5996. Heat Wave/Heat Wave.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int n, m, s, e;
+vector<vector<pii>> graph;
+int dist[2501];
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n >> m >> s >> e;
+    graph.resize(n + 1);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].emplace_back(b, c);
+        graph[b].emplace_back(a, c);
+    }
+}
+
+int dijkstra()
+{
+    priority_queue<pii> pq;
+    pq.emplace(0, s);
+
+    while (!pq.empty())
+    {
+        auto [d1, now] = pq.top();
+        d1 *= -1;
+        pq.pop();
+
+        if (now == e) return dist[now];
+
+        for (auto [next, d2]: graph[now])
+        {
+            int nd = d1 + d2;
+            if (dist[next] <= nd) continue;
+
+            dist[next] = nd;
+            pq.emplace(-nd, next);
+        }
+    }
+    return -1;
+}
+
+void solution()
+{
+    for (int i = 1; i <= n; i++) dist[i] = 2e9;
+    cout << dijkstra();
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
