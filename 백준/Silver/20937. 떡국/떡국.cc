66de463a79c5e1d6b9b563ccf06cb8762501cdc2
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-08-30 02:02:18 +0900

    [Silver IV] Title: 떡국, Time: 52 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Silver/20937. 떡국/떡국.cc
+++ 백준/Silver/20937. 떡국/떡국.cc
@@ -1,12 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 #define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
 
 int n;
 int cnt[50001];
@@ -20,18 +16,12 @@ void INPUT()
     {
         int size; cin >> size;
         cnt[size]++;
-        if (ans < cnt[size]) ans = cnt[size];
+        ans = max(ans, cnt[size]);
     }
-}
-
-
-void solution()
-{
-    cout << ans;
 }
 
 int main()
 {
     INPUT();
-    solution();
+    cout << ans;
 }
