be41eef6bdab55f43bd86a8bbdc050165e21c46d
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-09-07 01:32:20 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int n, m;
+vector<int> v;
+vector<int> comb;
+set<vector<int>> S;
+bool visited[9];
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        int num; cin >> num;
+        v.emplace_back(num);
+    }
+}
+
+void setAns()
+{
+    S.insert(comb);
+}
+
+
+void setComb(int depth, int start)
+{
+    if (depth == m)
+    {
+        setAns();
+        return;
+    }
+
+    for (int i = start; i < n; i++)
+    {
+        if (visited[i]) continue;
+
+        visited[i] = true;
+        comb.emplace_back(v[i]);
+        setComb(depth + 1, i + 1);
+        visited[i] = false;
+        comb.pop_back();
+    }
+}
+
+void solution()
+{
+    sort(v.begin(), v.end());
+    setComb(0, 0);
+    for (auto &s : S)
+    {
+        for (auto &i: s)
+            cout << i << " ";
+        cout << '\n';
+    }
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
