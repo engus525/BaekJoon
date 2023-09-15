2e1ce0a6e44e21b2784f5fe31a53b4211d720a24
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-09-15 23:13:12 +0900

    [Gold V] Title: 알약, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.cc
+++ 백준/Gold/4811. 알약/알약.cc
@@ -8,6 +8,7 @@ typedef pair<int, int> pii;
 typedef pair<long long, long long> pll;
 
 int n;
+ll dp[31][31];
 
 void INPUT()
 {
@@ -17,33 +18,21 @@ void INPUT()
 void solution()
 {
 
+    for (int half = 0; half <= 30; half++)
+    {
+        for (int one = 0; one <= 30; one++)
+        {
+            if (one < half) continue;
+
+            if (one == 0 || half == 0) dp[one][half] = 1;
+            else dp[one][half] = dp[one - 1][half] + dp[one][half - 1];
+        }
+    }
+
     while (cin >> n)
     {
         if (n == 0) break;
-
-        ll dp[62][31][31];
-        memset(dp, 0, sizeof dp);
-
-        dp[1][n - 1][1] = 1;
-        for (int i = 2; i <= 2 * n; i++)
-        {
-            for (int j = 0; j <= n; j++)
-            {
-                for (int k = 0; k <= n; k++)
-                {
-                    if (j + 1 <= n && k - 1 >= 0 && dp[i - 1][j + 1][k - 1])
-                        dp[i][j][k] += dp[i - 1][j + 1][k - 1];
-                    if (k + 1 <= n && dp[i - 1][j][k + 1])
-                        dp[i][j][k] += dp[i - 1][j][k + 1];
-                }
-            }
-        }
-
-        ll ans = 0;
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j <= n; j++)
-                ans += dp[2 * n][i][j];
-        cout << ans << '\n';
+        cout << dp[n][n] << '\n';
     }
 }
 
