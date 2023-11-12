118c0df672f39ebe8fadfabdd25319922889a559
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-11-13 01:51:42 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int a, b, c;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> a >> b >> c;
+}
+
+void solution()
+{
+    cout << max(b - a, c - b) - 1;
+}
+
+
+int main()
+{
+    INPUT();
+    solution();
+}
