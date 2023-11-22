086a87149fb969ffe0fca95da6f32fa33108ac0a
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-11-23 02:15:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.43 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    string answer;
+    
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == 'z') answer += '0', i += 3;
+        
+        else if (s[i] == 'o') answer += '1', i += 2;
+        
+        else if (s[i] == 't' && s[i + 1] == 'w') answer += '2', i += 2;
+        
+        else if (s[i] == 't') answer += '3', i += 4;
+        
+        else if (s[i] == 'f' && s[i + 1] == 'o') answer += '4', i += 3;
+        
+        else if (s[i] == 'f') answer += '5', i += 3;
+        
+        else if (s[i] == 's' && s[i + 1] == 'i') answer += '6', i += 2;
+        
+        else if (s[i] == 's') answer += '7', i += 4;
+        
+        else if (s[i] == 'e') answer += '8', i += 4;
+        
+        else if (s[i] == 'n') answer += '9', i += 3;
+        
+        else answer += s[i];
+        
+    }
+    
+    return stoi(answer);
+}
