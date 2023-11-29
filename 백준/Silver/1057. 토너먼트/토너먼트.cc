8e2fef92e9d58324b128e05ab0213336f5cd2b3b
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-11-30 01:36:39 +0900

    [Silver IV] Title: 토너먼트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.cc
+++ 백준/Silver/1057. 토너먼트/토너먼트.cc
@@ -2,9 +2,6 @@
 
 using namespace std;
 #define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
 
 int n, a, b;
 
@@ -16,7 +13,6 @@ void INPUT()
 
 void solution()
 {
-
     int ans = 1;
     while ((a + 1) / 2 != (b + 1) / 2)
     {
