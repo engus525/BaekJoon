da642d648d50109decbc4c2cf1e28535005db9ff
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-12-20 05:06:14 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+ll n;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+
+void solution()
+{
+    cout << n*n*n << '\n' << 3;
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
