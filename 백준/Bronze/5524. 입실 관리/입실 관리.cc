ae12e8e6f244238bcef10f223954f1a80b4bfe59
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2023-12-31 03:43:20 +0900

    [Bronze IV] Title: 입실 관리, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int n;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+void solution()
+{
+    while (n--)
+    {
+        string str;
+        cin >> str;
+        for (auto s : str)
+        {
+            if ('Z' >= s && s >= 'A') cout << char(s - 'A' + 'a');
+            else cout << char(s);
+        }
+        cout << '\n';
+    }
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
