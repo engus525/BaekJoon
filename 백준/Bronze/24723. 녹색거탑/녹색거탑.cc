764e1e131c8a9246f0ecfa40c2ae06c6f921145a
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-01-14 03:03:00 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int n;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+void solution()
+{
+    cout << pow(2,n);
+
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
