037fa45044f081bc340b9194161f120bbf2b1208
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-01-20 05:37:59 +0900

    [Bronze V] Title: The World Responds, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+
+void INPUT()
+{
+    IAMFAST
+}
+
+void solution()
+{
+
+    cout << "The world says hello!";
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
