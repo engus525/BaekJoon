53f716f2673e0dd720895955f8b33d49c13b1947
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-01-28 05:47:31 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int a,b;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> a >> b;
+}
+
+void solution()
+{
+    cout << min(a/2, b/2);
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
