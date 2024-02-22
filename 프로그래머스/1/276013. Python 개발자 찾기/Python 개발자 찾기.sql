296a197907f9eb60667da4a59a1fdac1e98fe850
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-02-23 04:33:26 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,7 +1,5 @@
 -- 코드를 작성해주세요
 select id, email, first_name, last_name
 from developer_infos
-where skill_1 like 'Python'
-or skill_2 like 'Python'
-or skill_3 like 'Python'
+where 'Python' in (skill_1, skill_2, skill_3)
 order by id
