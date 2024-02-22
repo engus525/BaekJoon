88571cdcf038ce713015e15054b614e2246aebc2
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-02-23 04:30:00 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,12 +1,8 @@
 -- 코드를 작성해주세요
 select iii.item_id, iii.item_name, iii.rarity
-from item_info ii, item_info iii, item_tree it
-where ii.item_id = it.parent_item_id and iii.item_id = it.item_id
-and ii.rarity like 'RARE'
-# and ii.rarity in
-# (
-#     select rarity
-#     from item_info
-#     where rarity like 'RARE'
-# )
+from item_info ii
+join item_tree it on ii.item_id = it.parent_item_id
+join item_info iii on it.item_id = iii.item_id
+# where ii.item_id = it.parent_item_id and iii.item_id = it.item_id
+where ii.rarity like 'RARE'
 order by iii.item_id desc
