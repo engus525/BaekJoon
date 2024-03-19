dedc37eb3809e2b4c1bbac473c3b8359932ab909
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-03-20 01:25:19 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+string str;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> str;
+}
+
+void solution()
+{
+    (str.substr(0, 3) == "555") ? cout << "YES" : cout << "NO";
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
