afdfe38b0e2cbc054212ada74c0a9684e22c193d
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-03-21 02:18:14 +0900

    [Gold V] Title: 벼락치기, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int dp[101][10001];
+int n, t;
+vector<pii> v;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n >> t;
+    v.emplace_back(0, 0);
+    for (int i = 0; i < n; i++)
+    {
+        int k, s;
+        cin >> k >> s;
+        v.emplace_back(k, s);
+    }
+}
+
+void solution()
+{
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= t; j++)
+        {
+            if (j - v[i].first < 0) dp[i][j] = dp[i - 1][j];
+            else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - v[i].first] + v[i].second);
+            ans = max(ans, dp[i][j]);
+        }
+    cout << ans;
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
