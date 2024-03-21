4fbafb332aa2e7655c8343a50a173e09c4836f40
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-03-22 02:05:35 +0900

    [Bronze V] Title: 分 (Minutes), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int a, b;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> a >> b;
+}
+
+void solution()
+{
+    cout << a * 60 + b;
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
