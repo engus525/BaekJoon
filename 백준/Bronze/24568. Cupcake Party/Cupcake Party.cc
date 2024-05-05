8300c5cc8ccb42d566b60713fb81e358e356c10b
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-05-06 04:07:00 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int a, b;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> a >> b;
+}
+
+void solution()
+{
+    cout << max(0, a * 8 + b * 3 - 28);
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
