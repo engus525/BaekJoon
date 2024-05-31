fc51b5d6b1c77ea17ce4f780ffa4c1ccc9f9863f
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-06-01 00:35:34 +0900

    [Bronze IV] Title: 計算 (Calculation), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24075. 計算 （Calculation）/計算 （Calculation）.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int a, b;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> a >> b;
+}
+
+void solution()
+{
+    cout << max(a + b, a - b) << '\n' << min(a + b, a - b);
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
