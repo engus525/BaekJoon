3088c1be1cc307d763ca701b70d260dd6cb857c9
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-07-23 14:06:59 +0900

    [Silver I] Title: 밤양갱, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31926. 밤양갱/밤양갱.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int n;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+void solution()
+{
+    int ans = 10;
+
+    ll cri = 1;
+    while (n >= cri * 2)
+    {
+//        cout << "cri : " << cri << '\n';
+        ans++;
+        cri *= 2;
+    }
+    cout << ans;
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
