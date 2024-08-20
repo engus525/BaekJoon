b29d2674afe1d7ed847bfbe53765c637ad88c466
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-08-21 03:59:15 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int n;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+void solution()
+{
+    while (n--) cout << "a";
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
