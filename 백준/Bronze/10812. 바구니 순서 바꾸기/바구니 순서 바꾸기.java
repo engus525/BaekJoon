27ceef228cd208c4a7233c36653b6ebc46b709d7
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-09-23 22:00:26 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 132 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int[] list = new int[101];
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+    for (int i = 1; i <= n; i++) {
+      list[i] = i;
+    }
+
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int l = Integer.parseInt(st.nextToken());
+      int r = Integer.parseInt(st.nextToken());
+      int mid = Integer.parseInt(st.nextToken());
+
+      Queue<Integer> q = new LinkedList<>();
+      for (int j = mid; j <= r; j++) q.add(list[j]);
+      for (int j = l; j < mid; j++) q.add(list[j]);
+      for (int j = l; j <= r; j++) {
+        list[j] = q.poll();
+      }
+    }
+
+    for (int i = 1; i <= n; i++) System.out.print(list[i] + " ");
+  }
+}
