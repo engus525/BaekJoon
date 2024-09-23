66e04408038ca4da145c542feac1b3ff5583c9b0
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-09-23 22:06:47 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.java
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,6 +13,7 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
     int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
@@ -32,6 +35,13 @@ public class Main {
       }
     }
 
-    for (int i = 1; i <= n; i++) System.out.print(list[i] + " ");
+    for (int i = 1; i <= n; i++) {
+      bw.write(String.valueOf(list[i]));
+      bw.write(" ");
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
   }
 }
