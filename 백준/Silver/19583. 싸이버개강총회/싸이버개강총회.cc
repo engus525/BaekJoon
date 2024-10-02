617778d8f9b283bfa8c026495089d4c443c665fc
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-10-03 00:10:34 +0900

    [Silver II] Title: 싸이버개강총회, Time: 80 ms, Memory: 12984 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.cc
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.cc
@@ -9,7 +9,6 @@ using namespace std;
 typedef long long ll;
 
 string s, e, q;
-int sh, sm, eh, em, qh, qm;
 set<string> nameSet;
 
 void INPUT()
@@ -18,36 +17,17 @@ void INPUT()
     cin >> s >> e >> q;
 }
 
-bool beforeS(string t)
-{
-    int h = stoi(t.substr(0, 2));
-    int m = stoi(t.substr(3, 2));
-    return ((h < sh) || (h == sh && m <= sm));
-}
-
-bool betweenEQ(string t)
-{
-    int h = stoi(t.substr(0, 2));
-    int m = stoi(t.substr(3, 2));
-    return ((eh < h) || (eh == h && em <= m)) && ((h < qh) || (h == qh && m <= qm));
-}
 
 void solution()
 {
-    sh = stoi(s.substr(0, 2));
-    sm = stoi(s.substr(3, 2));
-    eh = stoi(e.substr(0, 2));
-    em = stoi(e.substr(3, 2));
-    qh = stoi(q.substr(0, 2));
-    qm = stoi(q.substr(3, 2));
 
     int ans = 0;
 
     string t, name;
     while (cin >> t >> name)
     {
-        if (beforeS(t)) nameSet.emplace(name);
-        else if (betweenEQ(t))
+        if (t <= s) nameSet.emplace(name);
+        else if (e <= t && t <= q)
         {
             auto it = nameSet.find(name);
             if (it == nameSet.end()) continue;
