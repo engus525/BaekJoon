4632e6493fb2eadd66c3ec073825e40fd16c1606
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-10-03 00:45:25 +0900

    [Silver II] Title: 생태학, Time: 156 ms, Memory: 2292 KB -BaekjoonHub

---
+++ 백준/Silver/4358. 생태학/생태학.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <unordered_map>
+#include <string>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+
+string tree;
+int total = 0;
+map<string, int> treeMap;
+
+void INPUT()
+{
+    IAMFAST
+}
+
+
+void solution()
+{
+    while (getline(cin, tree))
+    {
+//        if (tree == "end") break;
+        total++;
+        treeMap[tree]++;
+    }
+
+
+    cout << fixed; cout.precision(4);
+    for (auto elem : treeMap)
+    {
+        double val = (elem.second / (double) total) * 100;
+        cout << elem.first << " " << val << '\n';
+    }
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
