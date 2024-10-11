6b547914aff0ec86c2ccf8f9df5dc7be329850fb
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-10-12 01:41:33 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <string>
+#include <memory.h>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int n;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n;
+}
+
+void solution()
+{
+    while (n--)
+    {
+        cout << "LoveisKoreaUniversity ";
+    }
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
