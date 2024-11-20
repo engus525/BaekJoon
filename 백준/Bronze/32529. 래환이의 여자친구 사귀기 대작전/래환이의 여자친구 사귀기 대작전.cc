6f9ba6ba4830acf0244a321e8900012890db2bb1
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2024-11-20 11:23:24 +0900

    [Bronze II] Title: 래환이의 여자친구 사귀기 대작전, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32529. 래환이의 여자친구 사귀기 대작전/래환이의 여자친구 사귀기 대작전.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <memory.h>
+#include <cmath>
+
+using namespace std;
+#define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+int n, m;
+vector<int> v;
+int ps = 0;
+int ans = 0;
+
+void INPUT()
+{
+    IAMFAST
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        int val; cin >> val;
+        v.emplace_back(val);
+        ps += val;
+        if (ps >= m)
+        {
+            ans = i + 1;
+            break;
+        }
+    }
+}
+
+
+void solution()
+{
+    if (ans) cout << ans;
+    else cout << -1;
+}
+
+int main()
+{
+    INPUT();
+    solution();
+}
