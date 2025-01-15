1ea353cc14c0a51abfeb7f481cb8d0109705579b
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-01-15 23:38:34 +0900

    [Silver III] Title: 거리의 합 2, Time: 88 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/23330. 거리의 합 2/거리의 합 2.cc
+++ 백준/Silver/23330. 거리의 합 2/거리의 합 2.cc
@@ -1,18 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <unordered_set>
-#include <set>
-#include <string>
 #include <algorithm>
-#include <memory.h>
-#include <cmath>
 
 using namespace std;
 #define IAMFAST ios_base::sync_with_stdio(false);cin.tie(0);
 typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<long long, long long> pll;
 
 int n;
 vector<ll> x, ps;
@@ -33,14 +25,8 @@ void solution()
    for (int i = 1; i < n; i++) ps[i] = ps[i - 1] + x[i];
 
    ll ans = 0;
-   for (int i = 0; i < n - 1; i++)
-   {
-//      cout << i << " -> x[" << i + 1 << "]  : " << x[i + 1] << " , ps[" << i << "] : " << ps[i] << '\n';
-//      cout << x[i + 1] << " " << ps[i] << '\n';
-//      cout << (i + 1) * x[i + 1] - ps[i] << '\n';
-      ans += (i + 1) * x[i + 1] - ps[i];
-   }
-   cout << ans * 2;
+   for (int i = 0; i < n - 1; i++) ans += (i + 1) * x[i + 1] - ps[i];
+   cout << 2 * ans;
 }
 
 
