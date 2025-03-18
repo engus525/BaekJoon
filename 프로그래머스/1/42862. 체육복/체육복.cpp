6e9b6e515c02ba3b08a07f7f3772720665bc61fd
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-03-18 16:41:07 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int cnt[31];
+bool noRest[31];
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    sort(lost.begin(), lost.end());
+    sort(reserve.begin(), reserve.end());
+    
+    for (auto val : lost) cnt[val]++;
+    for (auto val : reserve)
+    {
+        cnt[val]++;
+        // 여벌 학생이 도난 당하면 빌려줄 수 없다.
+        if (cnt[val] == 2)
+            noRest[val] = true;
+    }
+    int idx = 0;
+    
+    for (int i = 0; i < lost.size(); i++)
+    {
+        for (int j = 0; j < reserve.size(); j++)
+        {
+            if (noRest[reserve[j]]) continue;
+            if (cnt[lost[i]] == 2) continue;
+            
+            if (reserve[j] - 1 == lost[i] || reserve[j] + 1 == lost[i])
+            {
+                // cout << reserve[j] << " to " << lost[i] << '\n';
+                answer++;
+                noRest[reserve[j]] = true;
+                break;
+            }
+        }
+    }
+    
+    // 여벌 없고 도난 안 당한 학생
+    for (int i = 1; i <= n; i++)
+        if (!cnt[i]) answer++;
+    // cout << answer << " " << reserve.size();
+    return answer + reserve.size();
+}
