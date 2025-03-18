8a205baae9620cf4b08d49f867b961e88a01d171
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-03-18 14:39:36 +0900

    [level 1] Title: 최소직사각형, Time: 0.68 ms, Memory: 5.13 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -5,25 +5,14 @@
 using namespace std;
 typedef pair<int,int> pii;
 
-int solution(vector<vector<int>> sizes) {
-    vector<vector<int>> v;
-    for (auto size : sizes)
-    {
-        v.emplace_back(size);
-        v.push_back({size[1], size[0]});
-    }
+int solution(vector<vector<int>> v) {
     
-    pii ans = {v[0][0], v[0][1]};
-    for (int i = 2; i < v.size(); i += 2)
+    pii ans = {0, 0};
+    for (int i = 0; i < v.size(); i++)
     {
-        
-        pii ans1 = {max(v[i][0], ans.first), max(v[i][1], ans.second)};
-        pii ans2 = {max(v[i + 1][0], ans.first), max(v[i + 1][1], ans.second)};
-        ans = (ans1.first * ans1.second > ans2.first * ans2.second) ?
-            ans2 : ans1;
-        
+        ans.first = max(ans.first, max(v[i][0], v[i][1]));
+        ans.second = max(ans.second, min(v[i][0], v[i][1]));
     }
-    
     
     return ans.first * ans.second;
 }
