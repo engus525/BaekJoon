7b184a4c349c018c3e583415287e16c0ad13998d
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-03-18 14:53:15 +0900

    [level 1] Title: 모의고사, Time: 0.33 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> p1 = {1,2,3,4,5};
+vector<int> p2 = {2,1,2,3,2,4,2,5};
+vector<int> p3 = {3,3,1,1,2,2,4,4,5,5};
+int score[3];
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    
+    int maxScore = 0;
+    for (int i = 0; i < answers.size(); i++)
+    {
+        if (answers[i] == p1[i % p1.size()]) score[0]++;
+        if (answers[i] == p2[i % p2.size()]) score[1]++;
+        if (answers[i] == p3[i % p3.size()]) score[2]++;
+        maxScore = max(score[0], max(score[1], score[2]));
+    }
+    
+    for (int i = 0; i < 3; i++)
+        if (maxScore == score[i]) answer.emplace_back(i + 1);
+    return answer;
+}
