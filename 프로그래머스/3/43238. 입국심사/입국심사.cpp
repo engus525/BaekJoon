a2d41f49c7dbc0e6da0752e517f5690ca1bba87d
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-03-20 17:57:01 +0900

    [level 3] Title: 입국심사, Time: 43.44 ms, Memory: 6.86 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -9,13 +9,13 @@ typedef long long ll;
 ll solution(int n, vector<int> times) {
     ll answer = 2e18;
     
-    ll l = 0, r = 1e18;
+    ll l = 0, r = n * ll(*max_element(times.begin(), times.end()));
     while (l <= r)
     {
         ll mid = (l + r) / 2;
         
         ll pass = 0;
-        for (auto time : times) pass += mid / time;
+        for (auto time : times) pass += mid / ll(time);
         
         if (pass >= n)
         {
