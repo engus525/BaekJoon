cc0818b7bacf316ffef2d2f143fe1d7aa2f8ef54
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-04-09 00:24:27 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 10.89 ms, Memory: 28.9 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -7,6 +7,46 @@ using namespace std;
 vector<ll> v1, v2;
 ll ans;
 
+ll pulse(vector<ll> &v)
+{
+    int l = 0, r = 0;
+    ll temp = v[0];
+    while (l < v.size())
+    {
+        if (v[l] < 0)
+        {
+            l++, r++;
+            continue;
+        }
+        
+        temp = v[l];
+        ans = max(ans, temp);
+        
+        while (r + 1 < v.size())
+        {
+            if (v[l] < 0) break;
+            if (temp + v[r + 1] >= 0)
+            {
+                r++;
+                temp += v[r];
+                ans = max(ans, temp);
+                if (r >= v.size()) break;
+            }
+            else
+            {
+                l = r + 2;
+                r = l;
+                break;
+            }
+        }
+        
+        if (r + 1 >= v.size()) break;
+        
+    }
+    
+    return ans;
+}
+
 ll solution(vector<int> v) {
     
     for (int i = 0; i < v.size(); i++)
@@ -15,86 +55,6 @@ ll solution(vector<int> v) {
         else v1.emplace_back(-v[i]), v2.emplace_back(v[i]);
     }
     
-    int l = 0, r = 0;
-    ll temp = v1[0];
-    while (l < v.size())
-    {
-        if (v1[l] < 0)
-        {
-            l++, r++;
-            continue;
-        }
-        // cout << l << " , " << r << '\n';
-        
-        temp = v1[l];
-        ans = max(ans, temp);
-        
-        while (r + 1 < v.size())
-        {
-            if (v1[l] < 0) break;
-            // cout << "idx : " << l << " , " << r << '\n';
-            // cout << "val : " << v1[l] << " , " << v1[r] << '\n';
-            if (temp + v1[r + 1] >= 0)
-            {
-                // cout << "Move R\n";
-                r++;
-                temp += v1[r];
-                ans = max(ans, temp);
-                if (r >= v.size()) break;
-            }
-            else
-            {
-                // cout << "BREAK\n";
-                l = r + 2;
-                r = l;
-                break;
-            }
-        }
-        
-        if (r + 1 >= v.size()) break;
-        
-    }
     
-    l = 0, r = 0;
-    ll temp2 = v2[0];
-    while (l < v.size())
-    {
-        if (v2[l] < 0)
-        {
-            l++, r++;
-            continue;
-        }
-        // cout << l << " , " << r << '\n';
-        
-        temp2 = v2[l];
-        ans = max(ans, temp2);
-        
-        while (r + 1 < v.size())
-        {
-            if (v2[l] < 0) break;
-            // cout << "idx : " << l << " , " << r << '\n';
-            // cout << "val : " << v2[l] << " , " << v2[r] << '\n';
-            if (temp2 + v2[r + 1] >= 0)
-            {
-                // cout << "Move R\n";
-                r++;
-                temp2 += v2[r];
-                ans = max(ans, temp2);
-                if (r >= v.size()) break;
-            }
-            else
-            {
-                // cout << "BREAK\n";
-                l = r + 2;
-                r = l;
-                break;
-            }
-        }
-        
-        if (r + 1 >= v.size()) break;
-        
-    }
-    
-    
-    return ans;
+    return max(pulse(v1), pulse(v2));
 }
