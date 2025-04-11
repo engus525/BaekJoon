e3c32e74d24a3c6bfef87b8ae7ce7253c1fda371
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-04-11 14:44:43 +0900

    [level 4] Title: 도둑질, Time: 6.75 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -4,31 +4,23 @@
 
 using namespace std;
 
-int dp1[1'000'001][2];
-int dp2[1'000'001][2];
+int dp[1'000'001];
 
 
-int solution(vector<int> money) {
+int solution(vector<int> v) {
     int answer = 0;
     
-    dp1[0][1] = money[0];
-    for (int i = 1; i < money.size() - 1; i++)
-    {
-        dp1[i][0] = max(dp1[i - 1][0], dp1[i - 1][1]);
-        dp1[i][1] = dp1[i - 1][0] + money[i];
-        // cout << dp1[i][0] << " " << dp1[i][1] << '\n';
-        answer = max(dp1[i][0], dp1[i][1]);
-    }
+    dp[0] = dp[1] = v[0];
+    for (int i = 2; i < v.size() - 1; i++)
+        dp[i] = max(dp[i - 1], dp[i - 2] + v[i]);
+    answer = dp[v.size() - 2];
     
-    dp2[0][1] = money.back();
-    for (int i = money.size() - 1; i > 0; i--)
-    {
-        dp2[i][0] = max(dp2[i + 1][0], dp2[i + 1][1]);
-        dp2[i][1] = dp2[i + 1][0] + money[i];
-        // cout << dp2[i][0] << " " << dp2[i][1] << '\n';
-        answer = max(answer, max(dp2[i][0], dp2[i][1]));
-    }
     
+    dp[0] = 0;
+    dp[1] = v[1];
+    for (int i = 2; i < v.size(); i++)
+        dp[i] = max(dp[i - 1], dp[i - 2] + v[i]);
+    answer = max(answer, dp[v.size() - 1]);
     
     return answer;
 }
