c1850614c78d6c36f3a8d0739ac86ae00b810a64
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-04-11 11:34:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.05 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 unordered_map<string, int> cntM;
-unordered_map<string, vector<pair<int,int>>> numM;
+unordered_map<string, vector<int>> numM;
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
@@ -14,7 +14,7 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     for (int i = 0; i < genres.size(); i++)
     {
         cntM[genres[i]] += plays[i];
-        numM[genres[i]].emplace_back(plays[i], i);
+        numM[genres[i]].emplace_back(i);
     }
     
     vector<pair<string, int>> v(cntM.begin(), cntM.end());
@@ -24,24 +24,15 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
          });
     
     for (auto &[key, value] : numM) 
-        sort(value.begin(), value.end(), [](auto &a, auto &b)
+        sort(value.begin(), value.end(), [&plays](auto &a, auto &b)
              {
-                 return a.first > b.first;
+                 return plays[a] > plays[b];
              });
-    
-//     for (auto [key, value] : numM) 
-//     {
-//         for (auto val : value) cout << val.second << " ";
-//         cout << '\n';
-//     }
     
     
     for (auto val : v)
-    {
-        // for (auto num : numM[val.first]) answer.emplace_back(num);
         for (int i = 0; i < min(2, int(numM[val.first].size())); i++)
-            answer.emplace_back(numM[val.first][i].second);
-    }
+            answer.emplace_back(numM[val.first][i]);
     
     return answer;
 }
