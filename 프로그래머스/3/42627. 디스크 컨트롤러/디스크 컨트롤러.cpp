7d105cffc2540df7fa68ac85642ed6d9ce92dd65
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-04-16 16:52:36 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.15 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -23,41 +23,27 @@ int solution(vector<vector<int>> v) {
     sort(v.begin(), v.end());
     
     int vIdx = 0, nowTime = 0;
-    while (vIdx < v.size())
+    while (vIdx < v.size() || !pq.empty())
     {
         for (int i = vIdx; i < v.size(); i++)
         {
-            if (v[i][0] > nowTime) break;
+            if (v[i][0] > nowTime) continue;;
             vIdx++;
             pq.emplace(v[i][0], v[i][1]);
         }
         
-        if (vIdx < v.size() && pq.empty())
+        if (!pq.empty())
         {
-            // cout << "continue at " << nowTime << '\n';
+            auto [inTime, workTime] = pq.top();
+            pq.pop();
+            nowTime += workTime;
+            ans += nowTime - inTime;
+        }
+        else
+        {
             nowTime = v[vIdx][0];
-            continue;
         }
         
-        // cout << "qSize : " << pq.size() << '\n';
-        auto [inTime, workTime] = pq.top();
-        // cout << "NOW : " << nowTime << " => ";
-        // cout << inTime << " , " << workTime << '\n';
-        // cout << "vIdx : " << vIdx << '\n';
-        pq.pop();
-        nowTime += workTime;
-        ans += nowTime - inTime;
-    }
-    
-    while (!pq.empty())
-    {
-        auto [inTime, workTime] = pq.top();
-        // cout << "NOW : " << nowTime << " => ";
-        // cout << inTime << " , " << workTime << '\n';
-        // cout << "vIdx : " << vIdx << '\n';
-        pq.pop();
-        nowTime += workTime;
-        ans += nowTime - inTime;
     }
     
     return ans / v.size();
