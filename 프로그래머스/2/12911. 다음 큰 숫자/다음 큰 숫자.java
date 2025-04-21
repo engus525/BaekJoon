81b1ba33eeb2b64145e4ec7e94ac42738775def5
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-04-21 19:01:52 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,20 +2,11 @@ class Solution {
     public int solution(int n) {
         int ans = n + 1;
         
-        int oneCnt = 0;
-        String nString = Integer.toString(n, 2);
-        for (int i = 0; i < nString.length(); i++)
-            if (nString.charAt(i) == '1')
-                oneCnt++;
+        int oneCnt = Integer.bitCount(n);
         
         while (true)
         {
-            int cnt = 0;
-            String str = Integer.toString(ans, 2);
-            for (int i = 0; i < str.length(); i++)
-                if (str.charAt(i) == '1')
-                    cnt++;
-            if (oneCnt == cnt) break;
+            if (oneCnt == Integer.bitCount(ans)) break;
             ans++;
         }
         
