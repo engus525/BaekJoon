796bae4f9adf09692746f930e48ec4dced33ef36
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-05-03 22:00:34 +0900

    [level 3] Title: 인사고과, Time: 126.10 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,48 +2,22 @@ import java.util.*;
 
 class Solution {
     
-    boolean[] banned = new boolean[100001];
-    
-    class pii implements Comparable<pii>
-    {
-        int first;
-        int second;
-        pii(int f, int s){this.first=f;this.second=s;}
-        
-        @Override
-        public int compareTo(pii that) {
-            if (this.first == that.first) return this.second - that.second;
-            return that.first - this.first;
-        }
-        
-        @Override
-        public boolean equals(Object obj) {
-            pii that = (pii) obj;
-            return this.first == that.first && this.second == that.second;
-        }
-    }
-    
     public int solution(int[][] scores) {
         int ans = 1;
-        pii my = new pii(scores[0][0], scores[0][1]);
-        int mySum = my.first + my.second;
+        int[] my = scores[0];
+        int mySum = my[0] + my[1];
         
-        List<pii> v = new ArrayList<>();
-        for (int i = 0; i < scores.length; i++)
-        {
-            v.add(new pii(scores[i][0], scores[i][1]));
-        }
-        Collections.sort(v);
+        Arrays.sort(scores, (a,b)->(a[0]==b[0]?a[1]-b[1]:b[0]-a[0]));
         
         
         int maxScore = 0;
-        for (int i = 0; i < v.size(); i++)
+        for (int i = 0; i < scores.length; i++)
         {
-            pii score = v.get(i);
-            if (maxScore <= score.second)
+            int[] score = scores[i];
+            if (maxScore <= score[1])
             {
-                maxScore = score.second;
-                if (mySum < score.first + score.second) ans++;
+                maxScore = score[1];
+                if (mySum < score[0] + score[1]) ans++;
             }
             else if (score.equals(my)) return -1;
         }
