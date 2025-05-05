a5f4c098e0217832b171710a063fb3ed97e97c52
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-05-05 23:21:08 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    
+    public int solution(int n) {
+        int ans = 1;
+        
+        
+        while (n != 1)
+        {
+            if (n % 2 == 0) n /= 2;
+            else {
+                n--;
+                ans++;
+            }
+            // System.out.println(n);
+        }
+        
+
+        return ans;
+    }
+}
