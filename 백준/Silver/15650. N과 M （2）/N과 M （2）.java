ee332331091218f8bd7f21a4229480d8fbcc18d6
Kim Doo Hyeon <66135897+engus525@users.noreply.github.com>
2025-08-05 00:29:32 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 14740 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int n, m;
+  static List<Integer> list = new ArrayList<>();
+  static boolean[] visited = new boolean[10];
+
+  public static void comb(int start, int depth) {
+    if (depth == m) {
+      for (int num : list) System.out.print(num + " ");
+      System.out.println();
+      return;
+    }
+
+    for (int i = start; i <= n; i++) {
+      if (visited[i]) continue;
+
+      list.add(i);
+      visited[i] = true;
+      comb(i + 1, depth + 1);
+      list.remove(list.size() - 1);
+      visited[i] = false;
+    }
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    String input = br.readLine();
+    n = input.charAt(0) - '0';
+    m = input.charAt(2) - '0';
+
+    comb(1,0);
+
+  }
+}
